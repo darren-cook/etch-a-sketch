2687445569041b1b82239c0fbc64fa94187d23dd
darren-cook <darren.j.cook@uconn.edu>
2021-12-09 22:55:21 -0500

    html, css, and js basics setup.

---
+++ script.js
@@ -0,0 +1,51 @@
+const wrapper = document.querySelector(".wrapper");
+const gridSlide = document.querySelector("#gridSlide");
+const resetBtn = document.querySelector("#resetBtn");
+const toggleBtn = document.querySelector("#toggleBtn");
+
+let toggleCount = 0;
+
+makeGrid(gridSlide.value);
+
+function makeGrid (gridSize) {
+    for (let i=0; i < gridSize*gridSize; i++) {
+        wrapper.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+        wrapper.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+        let box = document.createElement("div");
+        box.className = "box";
+        box.id = i;
+        wrapper.appendChild(box)
+    }
+}
+
+function resetGrid(gridValue) {
+    while (wrapper.firstChild) {
+        wrapper.removeChild(wrapper.firstChild);
+    }
+    makeGrid(gridValue);
+    toggleCount = 0;
+}
+
+gridSlide.oninput = function(){
+    resetGrid(this.value);
+}
+
+resetBtn.addEventListener("click", function(){
+    resetGrid(16);
+    gridSlide.value = 16;
+})
+
+toggleBtn.addEventListener("click", function(){
+    const boxToggle = document.querySelectorAll(".box");
+    if (toggleCount%2 == 0){
+        boxToggle.forEach(function(item){
+            item.style.border = "none";
+        })
+    } else {
+        boxToggle.forEach(function(item){
+            item.style.border = "1px solid grey";
+        })       
+    }
+    toggleCount ++;
+})
+
